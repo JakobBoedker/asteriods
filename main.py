044fad7bb7daa6a0947931b6fa56207a78402830
jakobboedker <bdkerjakob1@gmail.com>
2024-10-28 16:02:40 +0100

    second commit

---
+++ main.py
@@ -0,0 +1,17 @@
+# this allow us to use code from
+# the open-source pygame library
+# throughout this file
+import os
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
+import pygame
+import constants
+
+
+def main():
+    print("Starting asteroids!")
+    print(f"Screen width: {constants.SCREEN_WIDTH}")
+    print(f"Screen height: {constants.SCREEN_HEIGHT}")
+
+
+if __name__ == "__main__":
+    main()
