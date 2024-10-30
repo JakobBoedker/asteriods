a9f9e46e4b38c82b4a1df530ad649dec72e573a8
jakobboedker <bdkerjakob1@gmail.com>
2024-10-30 22:24:07 +0100

    get a screen!

--- main.py
+++ main.py
@@ -1,17 +1,36 @@
 # this allow us to use code from
 # the open-source pygame library
 # throughout this file
-#does it work? git?
+# does it work? git?
 import os
-os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
+
+os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "hide"
 import pygame
-import constants
+from constants import *
 
 
 def main():
+    # Initialize Pygame
+    pygame.init()
+    # Set up the drawing window
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    #game loop
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        pygame.Surface.fill(screen, (0, 0, 0))
+        pygame.display.flip()
+
+    
+
+
+
     print("Starting asteroids!")
-    print(f"Screen width: {constants.SCREEN_WIDTH}")
-    print(f"Screen height: {constants.SCREEN_HEIGHT}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
 
 
 if __name__ == "__main__":
