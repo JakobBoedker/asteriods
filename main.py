92ef276ee454ac806cf9a1b1ebe8c5a4e5d7f6a0
jakobboedker <bdkerjakob1@gmail.com>
2024-11-05 22:03:56 +0100

    added forward/backwards movement and pygame sprite groups

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 # this allow us to use code from
 # the open-source pygame library
-# throughout this file
-# does it work? git?
 import os
+
 os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "hide"
 import pygame
+
 from constants import *
 from player import Player
 
@@ -13,33 +13,35 @@ def main():
     # Initialize Pygame
     pygame.init()
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
 
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    dt = 0
 
     # Set up the drawing window
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-    dt = 0
 
-
-    #game loop
+    # game loop
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
-
+        for obj in updatable:
+            obj.update(dt)
 
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
-
-    
-
 
 
 if __name__ == "__main__":
