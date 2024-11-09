bf89a5ab4224ae1c5fed75ca90cae6488ddde2f9
jakobboedker <bdkerjakob1@gmail.com>
2024-11-09 15:38:40 +0100

    added asteroids

--- main.py
+++ main.py
@@ -1,23 +1,29 @@
-# this allow us to use code from
-# the open-source pygame library
 import os
 
-os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "hide"
 import pygame
 
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 from constants import *
 from player import Player
 
+os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "hide"
+
 
 def main():
+    """game loop"""
     # Initialize Pygame
     pygame.init()
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroids_field = AsteroidField()
 
     dt = 0
 
